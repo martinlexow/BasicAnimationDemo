c92234342188284cbe41d5de2b238681d815dda5
Martin Lexow <martin.lexow@ixeau.com>
2020-11-26 18:23:46 +0100

    Basic Animation Demo

--- BasicAnimationDemo/ContentView.swift
+++ BasicAnimationDemo/ContentView.swift
@@ -1,13 +1,43 @@
-// ContentView.swift
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selected: Bool = false
+    @State var cardOffset: CGSize = .zero
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Rectangle()
+            .frame(width: 320, height: selected ? 80 : 200)
+            .cornerRadius(selected ? 42 : 12)
+            .foregroundColor(selected ? .pink : .gray)
+            .shadow(radius: 30)
+            .onTapGesture {
+                selected.toggle()
+            }
+            .scaleEffect(selected ? 1.0 : 0.5)
+            .rotationEffect(selected ? Angle(degrees: 15) : .zero)
+            .offset(cardOffset)
+            .animation(.interpolatingSpring(mass: 0.75,
+                                            stiffness: 400,
+                                            damping: 10,
+                                            initialVelocity: 0))
+            .gesture(
+                DragGesture()
+                    .onChanged({
+                        changedValue in
+                        cardOffset = changedValue.translation
+                    })
+                    .onEnded({
+                        _ in
+                        cardOffset = .zero
+                    })
+            )
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
